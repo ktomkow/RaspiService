8009429f74e401e3cb6c383bd1c15a33e8fe7cc8
ktomkow <krzysztof.tomkow@gmail.com>
2022-03-06 21:55:02 +0100

    Sample socketio service

--- raspi-service/index.js
+++ raspi-service/index.js
@@ -3,15 +3,20 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 app.get('/', function (req, res) {
-  res.sendfile('index.html');
+  res.send('<h1>Hello</h1>');
 });
 
+setInterval(() => {
+  console.log('Interval happened', new Date());
+  io.sockets.emit('greetings', { data: { text: 'hello' }, number: 5 });
+}, 5000);
+
 //Whenever someone connects this gets executed
-io.on('connection', function (socket) {
+io.on('connection', (socket) => {
   console.log('A user connected');
 
   //Whenever someone disconnects this piece of code executed
-  socket.on('disconnect', function () {
+  socket.on('disconnect', () => {
     console.log('A user disconnected');
   });
 });
