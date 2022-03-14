99e4bd4ba513d3ee6b441b36a0d349741c36eb29
ktomkow <krzysztof.tomkow@gmail.com>
2022-03-14 22:24:41 +0100

    Show cpu data on page

--- mobile-app/src/pages/Cpu.vue
+++ mobile-app/src/pages/Cpu.vue
@@ -1,24 +1,31 @@
 <template>
-  <q-page class="bg-red-1"> </q-page>
+  <q-page class="bg-red-1"> {{ cpuData }} </q-page>
 </template>
 
 <script>
-import { inject, onMounted, onUnmounted } from "vue";
+import { inject, onMounted, onUnmounted, toRefs, reactive } from "vue";
 
 export default {
   name: "Cpu",
   setup() {
+    const state = reactive({
+      cpuData: null,
+    });
+
     const emitter = inject("emitter");
 
     onMounted(() => {
       emitter.on("cpu", (data) => {
         console.warn("data", data);
+        state.cpuData = data;
       });
     });
 
     onUnmounted(() => {
       emitter.off("cpu");
     });
+
+    return { ...toRefs(state) };
   },
 };
 </script>
